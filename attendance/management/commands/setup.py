5ce9aaaf04fc8e18f8a534fb47c6b068b865dea8
David H <davidhefley@nebraska.edu>
2022-03-07 08:48:47 -0600

    update setup routine

--- attendance/management/commands/setup.py
+++ attendance/management/commands/setup.py
@@ -1,8 +1,12 @@
 import os
+from io import TextIOWrapper
 
 from decouple import config
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
+
+from attendance.utility import handle_uploaded_file
 
 
 class Command(BaseCommand):
@@ -12,27 +16,81 @@ class Command(BaseCommand):
         parser.add_argument('--mode', type=str, help="Mode")
 
     def handle(self, *args, **options):
-        self.stdout.write('Setting up Partnership 4 Kids Attendance System - this should only be run once!')
-        should_continue = input('Do you want to continue? [y/N]?')
-        if should_continue.lower() != 'y':
-            self.stdout.write('NOT continuing...')
-            return
-        migration_dir = 'attendance/migrations'
 
-        if ( config('DB') == 'SQLITE'):
-            if os.path.exists( 'db.sqlite3' ):
-                os.remove("db.sqlite3")
+        newdb = False
+
+        self.stdout.write('Setting up Partnership 4 Kids Attendance System.')
+        should_continue = input('Do you want to create a fresh DB? [y/N]?')
+        if should_continue.lower() != 'y':
+            self.stdout.write('Keeping existing DB')
+        else:
+            self.stdout.write('Removing existing DB')
+            if (config('DB').lower() == 'sqlite'):
+                if os.path.exists('db.sqlite3'):
+                    os.remove("db.sqlite3")
+                    self.stdout.write('SQL LITE DB REMOVED')
+
+                    newdb = True
+                else:
+                    self.stdout.write('sqlite db not found')
             else:
-                self.stdout.write('sqlite db not found')
+                self.stdout.write( "DB Configured to {}".format( config('DB') ) )
+
+        if not newdb:
+            should_continue = input('Do you want to create and apply migrations? [y/N]?')
+        else:
+            should_continue = 'y' #it was a new db, so force this!
+
+        if should_continue.lower() != 'y':
+            self.stdout.write('Not applying migrations')
+        else:
+            migration_dir = 'attendance/migrations'
             for f in os.listdir(migration_dir):
                 if f.startswith('__'):
                     continue
                 self.stdout.write(f)
 
-                os.remove( os.path.join(migration_dir, f) )
+                os.remove(os.path.join(migration_dir, f))
 
-        call_command("makemigrations", interactive=False)
-        call_command("migrate", interactive=False)
-        self.stdout.write('Creating superuser')
-        call_command("createsuperuser")
+            call_command("makemigrations", interactive=False)
+            call_command("migrate", interactive=False)
+
+        should_continue = input('Do you want to import sample data? [y/N]?')
+        if should_continue.lower() != 'y':
+            self.stdout.write('Not importing data')
+        else:
+            schools_file = 'attendance/tests/samples/schools.csv'
+            if not os.path.exists(schools_file):
+                self.stdout.write('School file not found')
+            else:
+                with open(schools_file, 'rb') as infile:
+                    _file = SimpleUploadedFile(schools_file, infile.read())
+                    result = handle_uploaded_file('school', TextIOWrapper(_file.file))
+                    if result['status'] == 'success':
+                        self.stdout.write('School file imported, importing classrooms and students')
+                        student_file = 'attendance/tests/samples/students.csv'
+                        if not os.path.exists(student_file):
+                            self.stdout.write('Student import file not found')
+                        else:
+                            with open(student_file, 'rb') as infile:
+                                _file = SimpleUploadedFile(student_file, infile.read())
+                                result = handle_uploaded_file('student', TextIOWrapper(_file.file))
+                                if result['status'] == 'success':
+                                    self.stdout.write('Students and classrooms file imported')
+                                else:
+                                    self.stdout.write('Error importing students and classrooms')
+                    else:
+                        self.stdout.write('Error import schools, skipping student/classroom import')
+
+        if not newdb:
+            should_continue = input('Do you want to create superuser account? [y/N]?')
+        else:
+            should_continue = 'y' #it was a new db, so force this!
+
+        if should_continue.lower() != 'y':
+            self.stdout.write('Not create superuser account')
+        else:
+            self.stdout.write('Creating superuser')
+            call_command("createsuperuser")
+
         self.stdout.write('Setup complete.')
--- readme.md
+++ readme.md
@@ -9,20 +9,20 @@ This project will be a student attendance tracking for partnership 4 kids.
   - Add the appropriate settings values.
   
 ### Initial Setup
-*DO THIS ONLY ONCE AT THE VERY BEGINNING OR TO START OVER!!*
+*YOU CAN ALSO RUN THIS TO START OVER!!*
 - First, run `python manage setup` 
   - This will:
-    1. Erase any migrations
-    2. Make the migrations fresh
-    3. Delete the mysql db if found
-    6. Prompt to create a new superuser
+    1. Prompt to erase sqlite db 
+    2. Prompt to erase any migrations, and make/apply the migrations fresh
+    3. Prompt to import sample data from test folder
+    4. Prompt to create a new superuser
 - Then, `python manage.py runserver`
 
 ## Adding views, forms, or admin:
 - Create a new file in the appropriate folder. For example, homepage.py in the view folder
 - Edit the __init__.py in that folder, and add "from .[filename] import *".  (don't forget the leading dot)
 
-## Importing date (for now)
+## Importing data (for now)
 - open `http://127.0.0.1:8000/import`
 - Select 'School File'
 - For the file, select 'p4kattendance\attendance\tests\samples\schools.csv'

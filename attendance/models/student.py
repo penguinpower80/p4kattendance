f708f1ff99f2f4d7ce88456a1408133477ffe24a
dhefley <dhefley@meridianne.com>
2022-03-12 14:24:11 -0600

    Increase size of school id field

--- attendance/models/student.py
+++ attendance/models/student.py
@@ -9,13 +9,13 @@ class Gender(models.TextChoices):
 
 
 class Student(models.Model):
-    nde_id = models.CharField(max_length=10, null=False, blank=False, primary_key=True)
+    nde_id = models.CharField(max_length=15, null=False, blank=False, primary_key=True)
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
     classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
     dob = models.DateField(null=True, blank=True)
     gender = models.TextField(max_length=1, choices=Gender.choices, default='', blank=True, null=True)
-    street = models.CharField(max_length=200, null=True, blank=True)
+    street = models.CharField(max_length11=200, null=True, blank=True)
     city = models.CharField(max_length=50, null=True, blank=True)
     state = models.CharField(max_length=2, null=True, blank=True)
     zip = models.CharField(max_length=20, null=True, blank=True)

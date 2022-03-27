dce32c5d8c53460cdbdf4d69db90c6013ec2b7c1
Diana Montano <38144610+Diana1928@users.noreply.github.com>
2022-03-27 15:26:17 -0500

    Adding change password

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 app_name = 'attendance'
 
 urlpatterns = [
           path('', views.home, name='home'),
+          path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password_change'),
           path('import', views.importfiles, name='import'),
           path('assignments', views.assignments, name='assignments'),
           path('assignments/<int:userid>', views.assign, name='assign'),

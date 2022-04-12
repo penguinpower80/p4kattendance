4a702344c5f57370a2457dc08f0745b7913579ed
Diana Montano <38144610+Diana1928@users.noreply.github.com>
2022-04-12 17:30:38 -0500

    Delete reports.py

--- templates/attendance/reports.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-
-@login_required
-def report(request):
-    return render(request, 'attendance/reports.html')
-

5180863c93e554775d49aa44fca32ac2e55381d3
Diana Montano <38144610+Diana1928@users.noreply.github.com>
2022-04-14 00:12:32 -0500

    Update reports.py

--- attendance/views/reports.py
+++ attendance/views/reports.py
@@ -1,10 +1,8 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.generic import ListView
 from attendance.models import Student
 
 
-@login_required
 class ReportListView(ListView):
     model = Student
     template_name = 'attendance/reports.html'

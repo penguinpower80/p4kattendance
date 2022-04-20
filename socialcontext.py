5000d6593ed2cef8ec5018529f3a330868b747cf
dhefley <dhefley@meridianne.com>
2022-04-20 09:44:47 -0500

    Social Test

--- socialcontext.py
+++ socialcontext.py
@@ -4,5 +4,7 @@ def socialproviders(request):
     return {
         'google': config('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY', default='') != '' and config('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET', default='') != '',
         'twitter': config('SOCIAL_AUTH_TWITTER_KEY', default='') != '' and config('SOCIAL_AUTH_TWITTER_SECRET', default='') != '',
-        'facebook': config('SOCIAL_AUTH_FACEBOOK_KEY', default='') != '' and config('SOCIAL_AUTH_FACEBOOK_SECRET', default='') != ''
+        'facebook': config('SOCIAL_AUTH_FACEBOOK_KEY', default='') != '' and config('SOCIAL_AUTH_FACEBOOK_SECRET', default='') != '',
+        'message': request.GET.get('message',''),
+        'backend': request.GET.get('backend', '')
     }

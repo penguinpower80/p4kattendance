66472aaa9b9e12262bc973769a14e03f37fa4962
David H <davidhefley@nebraska.edu>
2022-05-03 08:06:13 -0500

    Small update to email test function.

--- attendance/management/commands/emailtest.py
+++ attendance/management/commands/emailtest.py
@@ -13,20 +13,30 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('to', nargs='+', help="Email address to send to")
-        parser.add_argument('from', nargs='+', help="Email address to send from")
+        parser.add_argument('from', help="Email address to send from")
 
     def handle(self, *args, **options):
+
+        print(options)
+
         if options['to'] and options['from']:
             self.stdout.write('Sending test email to {} from {}'.format( options['to'], options['from']) )
-        send_mail(
-            'Test from p4k',
-            'Here is the sample message.',
-            options['to'],
-            [ options['from'] ],
-            fail_silently=False,
-        )
+        try:
+            send_mail(
+                'Test from p4k',
+                'Here is the sample message.',
+                options['from'],
+                options['to'],
+                fail_silently=False,
+            )
 
-        self.stdout.write('Sent using:{}'.format(settings.EMAIL))
+            self.stdout.write('Sent using:{}'.format(settings.EMAIL))
 
-        if ( settings.EMAIL == 'LOCAL' ):
-            self.stdout.write('Check "/{}" for emails'.format( settings.EMAIL_FILE_PATH) )
+            if (settings.EMAIL == 'LOCAL'):
+                self.stdout.write('Check "/{}" for emails'.format(settings.EMAIL_FILE_PATH))
+
+        except Exception as e:
+            print( str(e) )
+
+
+
